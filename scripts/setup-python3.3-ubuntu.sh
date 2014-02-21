7a2006b3b5f7cc26c35b115be896dc9c250560f4
Bjarki Agust Gudmundsson <bjarkig12@ru.is>
2014-02-21 04:47:22 +0000

    Update.

--- scripts/setup-python3.3-ubuntu.sh
+++ scripts/setup-python3.3-ubuntu.sh
@@ -1,17 +1,21 @@
+#!/bin/bash
 
-mkdir python33
-cd python33
+if ! hash python3.3 2>/dev/null
+then
+    mkdir python33
+    cd python33
 
-sudo apt-get install libreadline-dev libsqlite3-dev libbz2-dev libgdbm-dev liblzma-dev
+    sudo apt-get install libreadline-dev libsqlite3-dev libbz2-dev libgdbm-dev liblzma-dev
 
-wget http://www.python.org/ftp/python/3.3.4/Python-3.3.4.tgz
-tar xvf Python-3.3.4.tgz
-cd Python-3.3.4
+    wget http://www.python.org/ftp/python/3.3.4/Python-3.3.4.tgz
+    tar xvf Python-3.3.4.tgz
+    cd Python-3.3.4
 
-./configure
-make
-sudo make install
+    ./configure
+    make
+    sudo make install
 
-cd ..
-rm -rf python33
+    cd ..
+    rm -rf python33
+fi
 
