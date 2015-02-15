dae659563d5999b629cce26f5411ed6d6ef04363
James Elías <jamiees2@gmail.com>
2015-02-15 11:39:51 +0000

    removed jail code from entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,21 +5,6 @@ set -e
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 eval $(python3 $BASE_DIR/../config/config.py export)
 
-echo "Mounting directories for jail...."
-export JAIL=${EPSILON_JAIL:-$EPSILON_PREFIX/judge/jail}
-mkdir -p $JAIL/run
-sudo mount -o bind /run $JAIL/run
-
-mkdir -p $JAIL/proc
-sudo mount -o bind /proc $JAIL/proc
-
-mkdir -p $JAIL/dev/shm
-# This is somewhat insecure, but may use less memory
-# sudo mount -o bind /dev/shm $JAIL/dev/shm
-sudo mount -t tmpfs tmpfs $JAIL/dev/shm
-
-echo "done"
-
 # If we are supposed to load a contest, setup the aliases
 if ! [ -z $CONTEST ]; then
     export CONTEST_PATH=$(cd $CONTEST && pwd)
